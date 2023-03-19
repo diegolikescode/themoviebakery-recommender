35c2c44281cb2d46e2b0fa9e1faa895688b58b77
Diego Prestes <diegoprestes.git@hotmail.com>
2023-03-19 16:52:06 -0300

    add newMovieId column

--- src/data-preprocessing/b_newMovieId.py
+++ src/data-preprocessing/b_newMovieId.py
@@ -1,9 +1,18 @@
 # set newMovieId to the ratings dataset
 import pandas as pd
 import numpy as np
-import json
 
 # movies = json.load(open('../../data/treated-data/movies_kaggleId.json'))
 ratings_df = pd.read_csv('../../data/data-for-analysis/ratings-smaller.csv')
+ratings_df['newMovieId'] = ratings_df['movieId'].copy()
 
+unique_movies = ratings_df['movieId'].unique()
+new_ids = [0] * len(unique_movies)
+movie2newId = {}
+for i in range(len(new_ids)):
+    movie2newId[unique_movies[i]] = i
+    new_ids[i] = i
 
+ratings_df['newMovieId'].replace(movie2newId, inplace=True)
+
+print(ratings_df)
