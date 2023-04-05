8681cf22d864186fa934d49c224fdcd4a49cc66e
Diego Prestes <diegoprestes.git@hotmail.com>
2023-04-05 12:03:01 -0300

    prepare project to deploy; clear unused files; add requirements.txt

--- src/data_visualization/check.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-
-ratings_df = pd.read_csv('../../data/data-for-analysis/ratings-smaller-with-new-ids.csv')
-print(ratings_df)
--- src/data_visualization/ratings.py
+++ src/data_visualization/ratings.py
@@ -7,7 +7,8 @@ ratings_raw_df = pd.read_csv('../../data/raw-data/kaggle-ratings.csv')
 list_ratings_raw = np.sort(ratings_raw_df['rating'].unique(), kind='quicksort')
 raw_list_values = {}
 for rating_val in list_ratings_raw:
-    raw_list_values[rating_val] = ratings_raw_df[ratings_raw_df['rating'] == rating_val]['rating'].count()
+    raw_list_values[rating_val] = ratings_raw_df[ratings_raw_df['rating']
+                                                 == rating_val]['rating'].count()
 
 fig = plt.figure(figsize=(10, 7))
 plt.bar(list(list_ratings_raw), raw_list_values.values(), width=0.3)
@@ -20,7 +21,8 @@ ratings_full_df = pd.read_csv('../../data/treated-data/all-ratings.csv')
 list_ratings = np.sort(ratings_full_df['rating'].unique(), kind='quicksort')
 list_values = {}
 for rating_val in list_ratings:
-  list_values[rating_val] = ratings_full_df[ratings_full_df['rating'] == rating_val]['rating'].count()
+    list_values[rating_val] = ratings_full_df[ratings_full_df['rating']
+                                              == rating_val]['rating'].count()
 
 fig = plt.figure(figsize=(10, 7))
 plt.bar(list(list_ratings), list_values.values(), width=0.3)
@@ -28,14 +30,17 @@ plt.xlabel('notas de avaliação disponíveis')
 plt.ylabel('distribuição das notas')
 plt.show()
 
-ratings_smaller = pd.read_csv('../../data/data-for-analysis/ratings-smaller.csv')
+ratings_smaller = pd.read_csv(
+    '../../data/data-for-analysis/ratings-smaller.csv')
 
-list_ratings_smaller = np.sort(ratings_smaller['rating'].unique(), kind='quicksort')
+list_ratings_smaller = np.sort(
+    ratings_smaller['rating'].unique(), kind='quicksort')
 
 list_values_smaller = {}
 for rating_val in list_ratings_smaller:
 
-  list_values_smaller[rating_val] = ratings_smaller[ratings_smaller['rating'] == rating_val]['rating'].count()
+    list_values_smaller[rating_val] = ratings_smaller[ratings_smaller['rating']
+                                                      == rating_val]['rating'].count()
 
 fig = plt.figure(figsize=(10, 7))
 plt.bar(list(list_ratings_smaller), list_values_smaller.values(), width=0.3)
